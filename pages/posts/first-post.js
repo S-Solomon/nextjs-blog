023e5ff8c5c839d646f60f487bd13a1c9c32fde4
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-05-10 13:25:46 +0100

    first commit

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,1 +1,24 @@
-.js
+import Link from 'next/link' ;
+import Head from 'next/head' ;
+import Layout from '../../components/layout' ;
+
+
+
+export default function FirstPost() {
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>First Post</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                </Head>
+                <h1>First Post</h1>
+                <h2>
+                    <Link href="/">
+                        <a>Back to home</a>
+                    </Link>
+                </h2>
+            </Layout>
+        </>
+    )
+}
