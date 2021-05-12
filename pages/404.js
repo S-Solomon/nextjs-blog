1c10a8f7c37849597afa64c3267d70ab1978eecd
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-05-12 08:47:02 +0100

    added p tag

--- pages/404.js
+++ pages/404.js
@@ -9,6 +9,7 @@ export default function Custom404() {
     return (
         <>
         <h1>404 - Page Not Found</h1>
+        <p>oops lookd like you're lost in space</p>
         <div className={styles.backToHome}>
             <Link href="/">
                 <a>← Back to home</a>
