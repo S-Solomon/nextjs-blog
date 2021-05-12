2115edadb7fe7b7fe2c7766af58405302e0f328d
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-05-12 06:03:07 +0100

    added 404 page

---
+++ pages/404.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import styles from '../components/layout.module.css'
+
+
+
+
+
+export default function Custom404() {
+    return (
+        <>
+        <h1>404 - Page Not Found</h1>
+        <div className={styles.backToHome}>
+            <Link href="/">
+                <a>← Back to home</a>
+            </Link>
+        </div>
+        </>
+    )
+}
+
+
